b0cb39d73177e4a237dc58d0180818662292cbf5
Dmitrii_56 <ilin.dmitrii1984@gmail.com>
2018-11-07 06:50:30 +0300

    Переделал логику метода insert в части формирования размерности массива, capasity, в зависимости от кол-ва элементов, count

--- Din_massive_mod.py
+++ Din_massive_mod.py
@@ -31,11 +31,11 @@ class DynArray:
         self.count += 1
 
     def insert(self,i,itm):
-        new_capacity=self.capacity
+       # new_capacity=self.capacity
+        new_array=self.make_array(self.capacity)
         if (self.count)>=self.capacity:
-            new_capacity=2*self.capacity
-            self.resize(new_capacity)
-        new_array=self.make_array(new_capacity)
+            self.resize(2*self.capacity)
+            new_array=self.make_array(2*self.capacity)
         if i < 0 or i >= (self.count):
             raise IndexError('Index is out of bounds')
         for j in range(self.count):
